161fa8bfc1d6e0294d1dd0da023ad1b382b14925
hazlan <hazlan@datum.org>
2019-07-03 12:53:12 +0800

    initial commit

---
+++ src/main/kotlin/com/hazlanrozaimi/generest/GenerestApplication.kt
@@ -0,0 +1,11 @@
+package com.hazlanrozaimi.generest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GenerestApplication
+
+fun main(args: Array<String>) {
+	runApplication<GenerestApplication>(*args)
+}
